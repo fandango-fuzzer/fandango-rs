6102d7c862f46c5368256fb20e1b09b5951b1c86
Addison Crump <addison.crump@cispa.de>
2024-12-14 18:00:08 +0100

    larger benchmark suite

--- benches/generate.rs
+++ benches/generate.rs
@@ -23,7 +23,7 @@ fn graph_simple(c: &mut Criterion) {
 }
 
 mod simple {
-    use criterion::{black_box, Criterion};
+    use criterion::{black_box, BatchSize, BenchmarkId, Criterion, Throughput};
     use fandango_core::generation::util::Flattener;
     use fandango_core::generation::Generated;
     use fandango_core::typing::Node;
@@ -33,8 +33,11 @@ mod simple {
     };
     use fandango_core::visitor::write::WriteVisitor;
     use fandango_core::visitor::Visitor;
+    use fandango_core::visitor_chain;
     use fandango_derive::Fandango;
-    use rand::{thread_rng, Rng};
+    use rand::{thread_rng, Rng, SeedableRng};
+    use std::collections::BTreeMap;
+    use std::error::Error;
     use tuple_list::tuple_list;
 
     #[allow(dead_code)]
@@ -43,66 +46,100 @@ mod simple {
     pub struct Simple;
 
     pub fn simple(c: &mut Criterion) {
-        let mut rng = thread_rng();
-        c.bench_function("generate simple", |b| {
-            b.iter(|| nonterminal_start::generate(&mut rng, &mut ()))
-        });
-    }
+        let mut group = c.benchmark_group("simple");
 
-    pub fn visit_simple(c: &mut Criterion) {
-        let mut rng = thread_rng();
+        let rngs = (0..1000)
+            .map(|i| {
+                let mut rng = rand::rngs::StdRng::seed_from_u64(i);
+                let stashed = rng.clone();
 
-        c.bench_function("visit simple", |b| {
-            b.iter(|| {
-                let mut start = nonterminal_start::generate(&mut rng, &mut ());
-                let _ = WriteVisitor::cacheless(Vec::new())
-                    .visit(black_box(&mut start), 0)
-                    .unwrap()
-                    .continue_value()
-                    .unwrap()
-                    .output();
+                (
+                    nonterminal_start::generate(&mut rng, &mut ()).count_nodes(),
+                    stashed,
+                )
             })
-        });
-    }
+            .collect::<BTreeMap<_, _>>();
 
-    pub fn mutate_simple(c: &mut Criterion) {
-        let mut rng = thread_rng();
-        let mut start = Simple::extract("0").unwrap();
-        let mut count = start.count_nodes();
-        let mut generators = ();
+        for (count, mut rng) in rngs {
+            group.throughput(Throughput::Elements(count as u64));
 
-        c.bench_function("mutate simple", |b| {
-            b.iter(|| {
-                let old_start = start.clone();
-                let selection = rng.gen_range(0..count);
-                let mut path = Advance::forward(selection)
-                    .visit(&mut start, 0)
-                    .unwrap()
-                    .break_value()
-                    .unwrap();
-                let idx = path.pop_front().unwrap();
-                assert_eq!(0, idx);
-                let old_count = start.go_to(idx, path.clone()).unwrap().count_nodes();
-                let mutator = Mutator::new(&mut rng, &mut generators);
-                let new = mutator
-                    .starting_from(path)
-                    .visit(&mut start, idx)
-                    .unwrap()
-                    .break_value()
-                    .unwrap();
-                let new_count = new.count_nodes();
-                count = count - old_count + new_count;
-            })
-        });
+            group.bench_with_input(BenchmarkId::new("generate", count), &rng, |b, rng| {
+                b.iter_batched_ref(
+                    || rng.clone(),
+                    |rng| crate::simple::nonterminal_start::generate(rng, &mut ()),
+                    BatchSize::SmallInput,
+                );
+            });
+
+            let mut start = nonterminal_start::generate(&mut rng.clone(), &mut ());
+
+            group.bench_with_input(BenchmarkId::new("visit", count), &start, |b, start| {
+                b.iter_batched_ref(
+                    || start.clone(),
+                    |start| {
+                        WriteVisitor::cacheless(Vec::new())
+                            .visit(black_box(start), 0)
+                            .unwrap()
+                            .continue_value()
+                            .unwrap()
+                            .output()
+                    },
+                    BatchSize::SmallInput,
+                );
+            });
+
+            let mut count = start.count_nodes();
+
+            group.bench_with_input(BenchmarkId::new("mutate", count), &start, |b, start| {
+                b.iter_batched_ref(
+                    || start.clone(),
+                    |start| {
+                        let selection = rng.gen_range(0..count);
+                        let _: Result<(), Box<dyn Error>> = (|| {
+                            visitor_chain!(
+                                start,
+                                0,
+                                Advance::forward(selection),
+                                Mutator::new(&mut rng, &mut ())
+                            );
+                            Ok(())
+                        })();
+                    },
+                    BatchSize::SmallInput,
+                );
+            });
+        }
     }
 
     pub fn simple_flattened(c: &mut Criterion) {
-        let mut rng = thread_rng();
         let flattener = Flattener::new().flatten::<nonterminal_digit>().unwrap();
         let mut generators = tuple_list!(flattener);
-        c.bench_function("generate flattened", |b| {
-            b.iter(|| nonterminal_start::generate(&mut rng, &mut generators))
-        });
+
+        let mut group = c.benchmark_group("simple flattened");
+
+        let rngs = (0..1000)
+            .map(|i| {
+                let mut rng = rand::rngs::StdRng::seed_from_u64(i);
+                let stashed = rng.clone();
+
+                (
+                    nonterminal_start::generate(&mut rng, &mut generators).count_nodes(),
+                    stashed,
+                )
+            })
+            .collect::<BTreeMap<_, _>>();
+
+        for (count, rng) in rngs {
+            group.throughput(Throughput::Elements(count as u64));
+
+            group.bench_with_input(BenchmarkId::new("generate", count), &rng, |b, rng| {
+                b.iter_batched_ref(
+                    || rng.clone(),
+                    |rng| nonterminal_start::generate(rng, &mut generators),
+                    BatchSize::SmallInput,
+                );
+            });
+        }
     }
 }
 
@@ -125,12 +162,17 @@ fn graph_xml(c: &mut Criterion) {
 }
 
 mod xml {
-    use criterion::{black_box, Criterion};
+    use criterion::{black_box, BatchSize, BenchmarkId, Criterion, Throughput};
     use fandango_core::generation::Generated;
+    use fandango_core::visitor::mutator::Mutator;
+    use fandango_core::visitor::navigation::{Advance, CountNodes};
     use fandango_core::visitor::write::WriteVisitor;
     use fandango_core::visitor::Visitor;
+    use fandango_core::visitor_chain;
     use fandango_derive::Fandango;
-    use rand::thread_rng;
+    use rand::{thread_rng, Rng, SeedableRng};
+    use std::collections::BTreeMap;
+    use std::error::Error;
 
     #[allow(dead_code)]
     #[derive(Fandango)]
@@ -138,26 +180,69 @@ mod xml {
     pub struct Xml;
 
     pub fn xml(c: &mut Criterion) {
-        let mut rng = thread_rng();
-        c.bench_function("generate xml", |b| {
-            b.iter(|| nonterminal_start::generate(&mut rng, &mut ()))
-        });
-    }
+        let mut group = c.benchmark_group("xml");
 
-    pub fn visit_xml(c: &mut Criterion) {
-        let mut rng = thread_rng();
+        let rngs = (0..1000)
+            .map(|i| {
+                let mut rng = rand::rngs::StdRng::seed_from_u64(i);
+                let stashed = rng.clone();
 
-        c.bench_function("visit xml", |b| {
-            b.iter(|| {
-                let mut start = nonterminal_start::generate(&mut rng, &mut ());
-                let _ = WriteVisitor::cacheless(Vec::new())
-                    .visit(black_box(&mut start), 0)
-                    .unwrap()
-                    .continue_value()
-                    .unwrap()
-                    .output();
+                (
+                    nonterminal_start::generate(&mut rng, &mut ()).count_nodes(),
+                    stashed,
+                )
             })
-        });
+            .collect::<BTreeMap<_, _>>();
+
+        for (count, mut rng) in rngs {
+            group.throughput(Throughput::Elements(count as u64));
+
+            group.bench_with_input(BenchmarkId::new("generate", count), &rng, |b, rng| {
+                b.iter_batched_ref(
+                    || rng.clone(),
+                    |rng| crate::simple::nonterminal_start::generate(rng, &mut ()),
+                    BatchSize::SmallInput,
+                );
+            });
+
+            let mut start = nonterminal_start::generate(&mut rng.clone(), &mut ());
+
+            group.bench_with_input(BenchmarkId::new("visit", count), &start, |b, start| {
+                b.iter_batched_ref(
+                    || start.clone(),
+                    |start| {
+                        WriteVisitor::cacheless(Vec::new())
+                            .visit(start, 0)
+                            .unwrap()
+                            .continue_value()
+                            .unwrap()
+                            .output()
+                    },
+                    BatchSize::SmallInput,
+                );
+            });
+
+            let mut count = start.count_nodes();
+
+            group.bench_with_input(BenchmarkId::new("mutate", count), &start, |b, start| {
+                b.iter_batched_ref(
+                    || start.clone(),
+                    |start| {
+                        let selection = rng.gen_range(0..count);
+                        let _: Result<(), Box<dyn Error>> = (|| {
+                            visitor_chain!(
+                                start,
+                                0,
+                                Advance::forward(selection),
+                                Mutator::new(&mut rng, &mut ())
+                            );
+                            Ok(())
+                        })();
+                    },
+                    BatchSize::SmallInput,
+                );
+            });
+        }
     }
 }
 
@@ -168,10 +253,7 @@ criterion_group!(
     parse_xml,
     graph_xml,
     simple::simple,
-    simple::mutate_simple,
-    simple::visit_simple,
     simple::simple_flattened,
     xml::xml,
-    xml::visit_xml
 );
 criterion_main!(benches);
