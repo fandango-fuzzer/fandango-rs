941b1382429dd327930b4081a86182446396a7a0
Addison Crump <addison.crump@cispa.de>
2025-05-28 04:00:04 +0200

    whoops

--- scripts/build_time.sh
+++ scripts/build_time.sh
@@ -5,24 +5,24 @@ cd "baselines" || exit 1
 for target in csv rest scriptsizec xml; do
   cargo clean
   echo "Building ${target} (only dynamic, no optimizations)..."
-  time cargo build --quiet -p "${target}" --no-default-features --profile bench-noopt
+  time cargo build --quiet -p "${target}" --profile bench-noopt
 done
 
 
 for target in csv rest scriptsizec xml; do
   cargo clean
   echo "Building ${target} (only dynamic)..."
-  time cargo build --quiet -p "${target}" --no-default-features --profile bench
+  time cargo build --quiet -p "${target}" --profile bench
 done
 
 for target in csv rest scriptsizec xml; do
   cargo clean
   echo "Building ${target} (no optimizations)..."
-  time cargo build --quiet -p "${target}" --profile bench-noopt
+  time cargo build --quiet -p "${target}" --features static_defs --profile bench-noopt
 done
 
 for target in csv rest scriptsizec xml; do
   cargo clean
   echo "Building ${target}..."
-  time cargo build --quiet -p "${target}" --profile bench
+  time cargo build --quiet -p "${target}" --features static_defs --profile bench
 done
