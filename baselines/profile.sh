02f4b371cbedf6689f47207495828a2cc39726c9
Addison Crump <addison.crump@cispa.de>
2025-10-03 23:22:30 +0200

    rework profiling

---
+++ baselines/profile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+mkdir profiling-results
+RESULTS=$(realpath profiling-results)
+
+git submodule update --init --recursive
+cd fandango-prof
+
+# dependencies
+if ! [ -d ".venv" ]; then
+  git apply ../prof.diff
+  virtualenv .venv
+  . .venv/bin/activate
+  make install
+  pip install docutils tccbox
+else
+  . .venv/bin/activate
+fi
+
+# begin profiling
+for trial in $(seq 1 5); do
+  for target in {csv,rest,scriptsizec,xml}; do
+    echo "Beginning trial ${trial} for subject ${target}"
+    rm -f profile.json
+    mkdir -p "${RESULTS}/${target}/${trial}"
+    env PYTHONPATH=. python "evaluation/vs_isla/${target}_evaluation/${target}_evaluation.py" |& tee "${RESULTS}/${target}/${trial}/experiment_output.txt"
+    mv profile.json "${RESULTS}/${target}/${trial}/profile.json"
+  done
+done
